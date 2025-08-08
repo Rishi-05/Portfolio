e084dcb2d956e9fd4a989336e52258a44b37795a
Rishi Chilveri <115545106+Rishi-05@users.noreply.github.com>
2025-08-09 01:41:36 +0530

    Update ProjectsSection.tsx

--- components/Sections/ProjectsSection.tsx
+++ components/Sections/ProjectsSection.tsx
@@ -4,26 +4,23 @@ import Title from '../Title'
 
 const ProjectsSection = () => {
     const projects = {
-        PaisePlus: {
-            title: 'WealthWise',
-            description: 'A platform that simplifies financial planning, offering tools like simulators for investments and loans, personalized financial advice, and an AI-driven learning experience with courses, chatbots, and real-world challenges to strengthen financial decision-making skills',
-            link: 'https://devfolio.co/projects/paiseplus-07da',
-            image: '/WealthWise.png',
-            github: 'https://github.com/Mayur3sh007/PaisePlus'
+        WildVision: {
+            title: 'WildVision',
+            description: 'Animal classification system using MobileNetV2 and OpenCV, achieving over 90% accuracy on realworld wildlife and domestic animal images. Utilized OpenCV for image preprocessing, contour detection, and frame extraction, improving classification reliability by 35%.',
+            image: '',
+            github: 'https://github.com/Rishi-05/Animal-Image-Classification'
         },
-        vrSafe: {
-            title: 'vrSafe',
-            description: 'A fully functional Banking App developed with Next.js, delivering a suite of modern features designed to enhance your banking experience. From robust security to intuitive usability, our platform ensures you have the tools to manage your finances conveniently.',
-            link: 'https://vr-safe.vercel.app/',
-            image: '/vrSafe.png',
-            github: 'https://github.com/Mayur3sh007/vrSafe'
+        Text2Tone: {
+            title: 'Text2Tone',
+            description: 'Its an intuitive text-to-audio conversion platform using Flask, enabling users to convert written content into natural-sounding speech with support for multiple languages. Integrated Text-to-Speech APIs and optimized the backend pipeline, reducing audio generation time by 45%',
+            image:'',
+            github: ''
         },
-        CleanNGreen: {
-            title: 'Traffic Surveillance System',
-            description: 'An intelligent vehicle counting and detection system designed to provide precise counts within specified zones. It uses advanced object detection to optimize accuracy,  making it an efficient solution for traffic management and analysis',
-            link: 'https://www.linkedin.com/posts/mayuresh-chavan-04a3b5259_machinelearning-computervision-yolov8-activity-7174461543923785728-ICBS?utm_source=share&utm_medium=member_desktop',
-            image: '/cartracker.png',
-            github: 'https://github.com/Mayur3sh007/33_git-push--f_DUhacks'
+        CropSense: {
+            title: 'Crop-Recommendation-system',
+            description: 'A data-driven web application that helps farmers maximize their yield by recommending the most suitable crop based on real-time soil and weather conditions. This project leverages a machine learning model to provide accurate, actionable insights through a simple, user-friendly interface.',
+            image: '',
+            github: 'https://github.com/Rishi-05/Crop-Recommendation-system'
         }
     }
 
@@ -31,9 +28,9 @@ const ProjectsSection = () => {
         <div className='flex-col items-center scroll-mb-20 min-h-screen' id="projects">
             <Title word1="Featured" word2="PROJECTS" className='' />
             <section className="flex justify-center space-x-3 mb-10 max-md:flex-col max-md:-ml-2 ">
-                <Card {...projects.PaisePlus} />
-                <Card {...projects.vrSafe} />
-                <Card {...projects.CleanNGreen} />
+                <Card {...projects.WildVision} />
+                <Card {...projects.Text2Tone} />
+                <Card {...projects.CropSense} />
             </section>
         </div>
     )
