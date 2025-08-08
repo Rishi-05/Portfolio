11ac63a8959ba6fc3d1269bbf433e7634d82e82e
Rishi Chilveri <115545106+Rishi-05@users.noreply.github.com>
2025-08-09 02:43:07 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'mayuresh-chavan.vercel.app',
+                hostname: 'rishi-chilveri.vercel.app',
                 pathname: '/**',
             },
         ],
