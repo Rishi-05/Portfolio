36003cd759a27d088033221878ecfd6ffd4b1959
Rishi Chilveri <115545106+Rishi-05@users.noreply.github.com>
2025-11-05 23:44:46 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="py-8 px-4 bg-transparent text-white">
       <div className="max-w-7xl mx-auto  flex-col md:flex-row justify-center items-center">
         {/* Left Section */}
-        <div className="text-black text-center mb-4 md:mb-0 md:mr-4">
+        <div className="text-white text-center mb-4 md:mb-0 md:mr-4">
           <p>© 2024 Rishi Chilveri. All rights reserved.</p>
         </div>
       </div>
