46a1613f69c087eedd939a06d27073da30ce5571
Rishi Chilveri <115545106+Rishi-05@users.noreply.github.com>
2026-08-11 01:08:39 +0530

    Remove HeaderSection from page layout

--- app/page.tsx
+++ app/page.tsx
@@ -39,9 +39,6 @@ export default function Home() {
 
       <div className='w-full'>
 
-        <div className="max-md:hidden">
-          <HeaderSection />
-        </div>
         <HeroSection />
         <ProjectsSection />
 
