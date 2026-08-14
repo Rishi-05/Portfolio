b1129c973c786e0fbc01a1ba869651d6edd37195
Rishi Chilveri <115545106+Rishi-05@users.noreply.github.com>
2026-08-15 00:25:59 +0530

    Update Home component structure with new sections
    Refactored the Home component to include Hero, Projects, Skills, and Contact sections.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+/*
 import Title from '@/components/Title';
 import SkillCard from '@/components/SkillCard';
 import { Navbar } from '@/components/Navbar';
@@ -90,6 +91,28 @@ export default function Home() {
           <HeaderSection />
           <Skills/>
         </div>
+      </div>
+    </main>
+  );
+}
+*/
+
+import Hero from '@/components/Sections/Hero';
+import Projects from '@/components/Sections/Projects';
+import Skills from '@/components/Sections/Skills';
+import Contact from '@/components/Sections/Contact';
+import Nav from '@/components/Nav';
+
+export default function Home() {
+  return (
+    <main className='relative flex flex-col w-full h-full overflow-hidden mx-auto sm:px-10 px-5 z-50 '>
+      <Nav />
+
+      <div className='w-full'>
+        <Hero />
+        <Projects />
+        <Skills />
+        <Contact />
       </div>
     </main>
   );
